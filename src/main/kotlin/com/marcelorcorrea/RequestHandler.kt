730b67e657238827f62a325457a789a1d3309a49
Marcelo Correa <marcelorcorrea@gmail.com>
2017-06-19 15:57:30 -0300

    Move to a package

--- src/main/kotlin/com/marcelorcorrea/RequestHandler.kt
+++ src/main/kotlin/com/marcelorcorrea/RequestHandler.kt
@@ -1,3 +1,5 @@
+package com.marcelorcorrea
+
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import com.fasterxml.jackson.databind.DeserializationFeature
